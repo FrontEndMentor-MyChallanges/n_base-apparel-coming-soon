420fcc806c1edf322443e199531b1731778b9c2e
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-12 12:26:41 +0200

    Testing tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
 			backgroundImage: {
 				'custom-linear-white': 'linear-gradient(to right bottom, rgba(255,255,255,1) 0%, rgba(255,245,245,1) 100%);',
 				'custom-linear-tomato': 'linear-gradient(to right bottom, rgba(248,191,191,1) 0%, rgba(238,140,140,1) 100%);',
+				'pattern-desktop': 'url("../images/bg-pattern-desktop.svg")',
+				'hero-mobile': 'url("../images/hero-mobile.jpg")',
+				'hero-desktop': 'url("../images/hero-desktop.jpg")',
 			},
 			fontFamily: {
 				josefin: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
