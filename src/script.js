0574707ea09039a5ec8f489a1a8c889a1f83ea42
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-23 09:53:32 +0200

    Form logic updated

--- src/script.js
+++ src/script.js
@@ -3,20 +3,73 @@
 const form = document.querySelector('.form');
 const emailInput = document.querySelector('.input-email');
 const submitBtn = document.querySelector('.btn-submit');
-const errMsg = document.querySelector('.errorMsg');
+const errorMsg = document.querySelector('.errorMsg');
+const validationImg = document.querySelector('.imgValid');
 
-const isRequired = (value) => (value === '' ? false : true);
+const isRequired = value => (value === '' ? false : true);
 
-const isEmailValid = (email) => {
-  const emailRegex = new RegExp(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
+const isEmailValid = email => {
+	const emailRegex = new RegExp(
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	);
 
-  return emailRegex.test(email);
+	return emailRegex.test(email);
 };
 
 //////////////////////////
 
+const showError = function (message = '') {
+	emailInput.classList.add('border-red-600', 'text-red-600');
+	errorMsg.textContent = message;
+	errorMsg.classList.remove('hidden');
+	validationImg.classList.remove('hidden');
+};
+
+const hideError = function (message = '') {
+	emailInput.classList.remove('border-red-600', 'text-red-600');
+	errorMsg.textContent = message;
+	errorMsg.classList.add('hidden');
+	validationImg.classList.add('hidden');
+};
+
+const checkEmail = function () {
+	let isValid = false;
+
+	const email = emailInput.value.trim();
+	const validEmail = isEmailValid(email);
+
+	if (!isRequired(email)) {
+		showError('Email cannot be empty');
+	} else if (!validEmail) {
+		showError('Please provide a valid email');
+	} else {
+		isValid = true;
+	}
+
+	return isValid;
+};
+
+const validateForm = () => {
+	let isEmailValid = checkEmail();
+
+	let isFormValid = isEmailValid;
+
+	return isFormValid;
+};
+
 form.addEventListener('submit', function (e) {
-  e.preventDefault();
+	e.preventDefault();
+
+	let formValid = validateForm();
+
+	if (formValid) {
+		console.log('Form validated, can be send to server');
+		emailInput.value = '';
+	} else {
+		console.log('Something is wrong. Check inputs!');
+	}
+});
+
+emailInput.addEventListener('input', function () {
+	hideError();
 });
